72d8f430753390ce5e9a4d62cc0f8eaf1707e713
ahmedabdalkaderr <abdelkader6696@gmail.com>
2024-02-27 17:11:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+PORT=5000
+NODE_ENV=development
+
+# DateBase
+DB_USER=abdelkdaer-nofal
+DB_PASSWORD=6FFmIg7TbI9gURGa
+DB_NAME=lms-db
+DB_URI=mongodb+srv://abdelkdaer-nofal:6FFmIg7TbI9gURGa@cluster0.unxvehv.mongodb.net/lms?retryWrites=true&w=majority&appName=Cluster0
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", true);
+
+dotenv.config({ path: "config.env" });
+mongoose
+  .connect(process.env.DB_URI)
+  .then((con) => {
+    console.log(con.connection.host);
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit();
+  });
+
+const app = express();
+
+
+const PORT = process.env.PORT;
+
+
+
+app.use(express.json());
+
+// Middlewares
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log("mode: development");
+}
+
+const userSchema = mongoose.Schema({
+  name: String,
+});
+
+const UserModel = mongoose.model("User", userSchema);
+
+app.post("/", (req, res) => {
+  const name = req.body.name;
+  const newuser = new UserModel({ name });
+  newuser.save().then((doc) => {
+    console.log(doc);
+  });
+  res.send({ data: newuser });
+});
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Our API v1");
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
