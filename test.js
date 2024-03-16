0fdaf74a2627a64c52e7d4768dbd5cf8c4210510
ahmedabdalkaderr <abdelkader6696@gmail.com>
2024-03-16 23:54:08 +0200

    m

--- test.js
+++ test.js
@@ -1,8 +1,28 @@
 
+const getUsers = async function (query = "") {
+  try {
+    const res = await axios.get(`http://localhost:5000/api/v1/users${query}`);
+    console.log(res.data);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+  }
+};
 
-const getUsers = async function(){
-    const res = await fetch(`http://localhost:5000/api/v1/users`);
-    const data = await res.json();
-    console.log(data)
-}
-getUsers();
+const newData = {
+  instructor: "y",
+};
+
+const updateUser = async function () {
+  try {
+    const res = await axios
+      .put("http://localhost:5000/api/v1/courses/65f4f26f9854fa3d14702d4c", {
+        instructor: "a",
+      })
+   const data = await res.data;
+   console.log(data)
+  } catch (error) {
+    console.error("Error updating course:", error);
+  }
+};
+
+updateUser()
